bdb66b35e26fcba9737f4771e214a198f20a06cb
reeceslade <89296928+reeceslade@users.noreply.github.com>
2023-02-19 20:14:59 +0000

    Add files via upload

---
+++ Elanco/index.php
@@ -0,0 +1,42 @@
+<? // when i click the drop down it resets the url so that if i previously have ascending on time then click ascending on date itll do that ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Task Submission Software Engineer</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<style>
+    .welcome{
+      text-align:center;
+    }
+
+    .app{
+      text-align:center;
+      margin:100px
+    }
+
+    .img{
+      height: 100px
+      width: 100px
+    }
+  </style>
+</head>
+<div style="position: relative;">
+  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Elanco.svg/1200px-Elanco.svg.png" style="position: absolute; top: 0; left: 0; width: 150px;">
+  <img src="https://lightuppoole.co.uk/wp-content/uploads/2020/01/BU-portrait-logo.jpg" style="position: absolute; top: 0; right: 0; width: 150px;">
+  <div style="margin-top: 50px;">
+    <div class="welcome">
+      <h3>API Solution</h3>
+      <p>By Reece Slade</p>
+    </div>
+    <div class="app">
+      <a href="applications.php">Applications</a>
+    </div>
+    <div class="app">
+      <a href="resources.php">Resources</a>
+    </div>
+  </div>
+</div>
+</body>
+</html>
---
+++ Elanco/resources.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Elanco Applications</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<style>
+
+    img {
+      height: 75px;
+      width: 75px;
+      position: fixed;
+      top: 0;
+      right: 0;
+      margine:100px;
+    }
+    
+		table {
+			border-collapse: collapse;
+			width: 100%;
+		}
+
+		th, td {
+			text-align: left;
+			padding: 8px;
+		}
+
+		tr:nth-child(even){background-color: #f2f2f2}
+
+		th {
+			background-color: #4CAF50;
+			color: white;
+		}
+	</style>
+</head>
+<body>
+      
+<?php
+$api_url = 'https://engineering-task.elancoapps.com/api/resources/';
+$applications = json_decode(file_get_contents($api_url), true);
+
+$order = isset($_GET['order']) ? $_GET['order'] : '';
+
+$application_name = isset($_GET['resource-name']) ? $_GET['resource-name'] : '';
+
+if (!empty($_GET['resource-name'])) {
+  $resource_name = str_replace(' ', '%20', $_GET['resource-name']);
+  $resources_url = $api_url . $resource_name;
+  
+  $resources = json_decode(file_get_contents($resources_url), true);
+  
+  if (!empty($application_name)) {
+    $resources_url = $api_url . $resource_name;
+    $resources = json_decode(file_get_contents($resources_url), true);
+
+    if ($order === 'ascending-qty') {
+      usort($resources, function ($a, $b) {
+        return $b['ConsumedQuantity'] <=> $a['ConsumedQuantity'];
+      });
+    } else if ($order === 'descending-qty') {
+      usort($resources, function ($a, $b) {
+        return $a['ConsumedQuantity'] <=> $b['ConsumedQuantity'];
+      });
+    }
+  
+      if ($order === 'ascending-cost') {
+        usort($resources, function ($a, $b) {
+          return $b['Cost'] <=> $a['Cost'];
+        });
+      } else if ($order === 'descending-cost') {
+        usort($resources, function ($a, $b) {
+          return $a['Cost'] <=> $b['Cost'];
+        });
+      }
+          if ($order === 'ascending-date') {
+            usort($resources, function ($a, $b) {
+              return $b['Date'] <=> $a['Date'];
+            });
+          } else if ($order === 'descending-date') {
+            usort($resources, function ($a, $b) {
+              return $a['Date'] <=> $b['Date'];
+            });
+          }
+        }
+  ?>
+	<h2><?php echo $_GET['resource-name'];?></h2>
+  <a href="index.php">
+  <img src="https://cdn-icons-png.flaticon.com/512/25/25694.png" alt="Icon">
+</a>
+	<table>
+		<tr>
+  <th>Consumed Quantity
+   <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="GET">
+                    <input type="hidden" name="resource-name" value="<?php echo $application_name; ?>">
+                    <select name="order" onchange="this.form.submit()">
+                        <option value="" disabled selected>Choose an option</option>
+                        <option value="ascending-qty" <?php if ($order === 'ascending-qty') echo 'selected'; ?>>Ascending</option>
+                        <option value="descending-qty" <?php if ($order === 'descending-qty') echo 'selected'; ?>>Descending</option>
+                </select>
+               </form>
+            </th>
+			<th>Cost
+        <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="GET">
+          <input type="hidden" name="resource-name" value="<?php echo $application_name; ?>">
+          <select name="order" onchange="this.form.submit()">
+            <option value="" disabled selected>Choose an option</option>
+            <option value="ascending-cost" <?php if ($order === 'ascending-cost') echo 'selected'; ?>>Ascending</option>
+            <option value="descending-cost" <?php if ($order === 'descending-cost') echo 'selected'; ?>>Descending</option>
+          </select>
+        </form>
+      </th>
+			<th>Date
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="GET">
+          <input type="hidden" name="resource-name" value="<?php echo $application_name; ?>">
+          <select name="order" onchange="this.form.submit()">
+            <option value="" disabled selected>Choose an option</option>
+            <option value="ascending-date" <?php if ($order === 'ascending-date') echo 'selected'; ?>>Ascending</option>
+            <option value="descending-date" <?php if ($order === 'descending-date') echo 'selected'; ?>>Descending</option>
+          </select>
+        </form></th>
+			<th>Instance ID</th>
+			<th>Meter Category</th>
+			<th>Resource Group</th>
+			<th>Resource Location</th>
+			<th>Tags</th>
+			<th>Unit of Measure</th>
+			<th>Location</th>
+			<th>Service Name</th>
+		</tr>
+		<?php foreach ($resources as $resource) { ?>
+			<tr>
+				<td><?php echo $resource['ConsumedQuantity']; ?></td>
+				<td><?php echo $resource['Cost']; ?></td>
+				<td><?php echo $resource['Date']; ?></td>
+				<td><?php echo $resource['InstanceId']; ?></td> 
+				<td><?php echo $resource['MeterCategory']; ?></td>
+        <td>  <?php $meter_category_url = 'https://elanco.reeceslade.repl.co/applications.php?application-name=' . urlencode($resource['ResourceGroup']); ?>
+      <a href="<?php echo $meter_category_url; ?>"><?php echo $resource['ResourceGroup']; ?></a></td>
+				<td><?php echo $resource['ResourceLocation']; ?></td><td>
+    <?php 
+      $tags = $resource['Tags'];
+      $formattedTags = '';
+      foreach ($tags as $key => $value) {
+        $formattedTags .= $key . ': ' . $value . '<br>';
+      }
+      echo $formattedTags; ?>
+     </td>
+				<td><?php echo $resource['UnitOfMeasure']; ?></td>
+				<td><?php echo $resource['Location']; ?></td>
+				<td><?php echo $resource['ServiceName']; ?></td>
+			</tr>
+		<?php } ?>
+	</table>
+<?php
+}
+?>
+<h2>Resources</h2>
+  <a href="index.php">
+  <img src="https://cdn-icons-png.flaticon.com/512/25/25694.png" alt="Icon">
+</a>
+<table>
+	<tr>
+		<th>Resource Name</th>
+		<th>Actions</th>
+	</tr>
+	<?php foreach ($applications as $application) { ?>
+		<tr>
+			<td><?php echo $application; ?></td>
+			<td>
+				<form action="" method="GET">
+					<input type="hidden" name="resource-name" value="<?php echo $application; 
+          //loop through application url and put the names into a table then whe we click view resources?>">
+					<button type="submit">View Resources</button>
+				</form>
+			</td>
+		</tr>
+	<?php } ?>
+</table>
+</body>
+</html>
